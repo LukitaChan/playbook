af2dad598ff9f733fb3ea6a3665ecd8ad6aaab8b
LukitaChan <kamery_94@hotmail.com>
2022-05-02 17:16:20 -0500

    API_UserTable

--- weekly_mission_4/1_Refactoring/lib/test/FizzbuzzService.test.js
+++ weekly_mission_4/1_Refactoring/lib/test/FizzbuzzService.test.js
@@ -11,19 +11,19 @@ describe('Test para funcion FizzbuzzService', () => {
   });
 
   test('2. Test para applyValidationInExplorer', () => {
-		const explorersFBS = { score: 3 };
+		const explorersFBS = { "score": 3 };
 		const explorers35 = FizzbuzzService.applyValidationInExplorer(explorersFBS);
 		expect(explorers35.trick).toBe("FIZZ");
 	});
 
   test('3. Test para applyValidationInExplorer', () => {
-		const explorersFBS = { score: 5 };
+		const explorersFBS = { "score": 5 };
 		const explorers35 = FizzbuzzService.applyValidationInExplorer(explorersFBS);
 		expect(explorers35.trick).toBe('BUZZ');
 	});
 
   test('4. Test para applyValidationInExplorer', () => {
-		const explorersFBS = { score: 15 };
+		const explorersFBS = { "score": 15 };
 		const explorers35 = FizzbuzzService.applyValidationInExplorer(explorersFBS);
 		expect(explorers35.trick).toBe('FIZZBUZZ');
 	});
